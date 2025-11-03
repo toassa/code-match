d4692448efac0593ae3f862f91ba13236f5eeecc
Wesley Henrique Batista Santanna <w284045@ftad.ft.unicamp.br>
2025-11-03 20:36:15 -0300

    feat: definição das variáveis

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,13 +1,6 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db_name = "code_match_bd";
-    
-    
-    $conn = new mysqli($host, $user, $pass, $db_name);
-
-    if ($conn->connect_error) { 
-        die("". $conn->connect_error);
-    }
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "code_match_bd";
 ?>
